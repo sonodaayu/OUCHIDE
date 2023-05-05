4de0792193ddc07c048c362a938a1d568f640078
sonodaayu <sonoda@west.sd.keio.ac.jp>
2023-05-05 22:50:53 +0900

    render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,9 @@ set -o errexit
 
 bundle config set force_ruby_platform true
 bundle install
+yarn install
+yarn build
+bundle exec rake assets:precompile
 bundle exec rails db:migrate
 bundle exec rails db:seed
 bundle exec rails assets:precompile
